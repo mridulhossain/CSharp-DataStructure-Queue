5d58801dfe7daa0cdec17fca5c5e5e51af7699ec
mridulhossain <mridulfeni94@gmail.com>
2021-09-20 01:08:26 +0600

    csharp queue

---
+++ OOP/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Queue work fifo style(first in first out)
+            // if you want to insert data in queue you have to use dataset.Enqueue(value) method.
+            // if you want to see first data you can use peek method,like - dataset.Peek();
+            //
+
+            Queue<int> numbers = new Queue<int>();
+
+            // you can add data by using enqueue method like this.
+            numbers.Enqueue(1);
+            numbers.Enqueue(2);
+            numbers.Enqueue(3);
+            numbers.Enqueue(4);
+
+            // check first data by using peek method
+            Console.WriteLine(numbers.Peek());
+
+            // you can remove first data by using dequeue method
+            numbers.Dequeue();
+
+            // let's check,remove or not.
+            Console.WriteLine(numbers.Peek());
+
+            // if you want to check exist or not,you can use contains method,it return boolean 
+            Console.WriteLine(numbers.Contains(4));
+
+
+            // you can count your dataset.
+            Console.WriteLine(numbers.Count);
+
+            //you can clear this data set using clear.
+            numbers.Clear();
+        }
+    }
+}
+
+
